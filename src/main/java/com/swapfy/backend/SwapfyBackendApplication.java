2b59e76c29cc309bfbf4818ecf9497afbb6a58cd
Marr03 <msiegut@g.educaand.es>
2025-03-24 20:23:26 +0100

    Initial commit of Swapfy backend project

---
+++ src/main/java/com/swapfy/backend/SwapfyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swapfy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapfyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwapfyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swapfy/backend/SwapfyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapfy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapfyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
