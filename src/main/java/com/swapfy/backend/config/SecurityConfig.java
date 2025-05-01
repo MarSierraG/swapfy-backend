0fbd40e58b620707edaf54190398f0f084652ae6
Marr03 <msiegut@g.educaand.es>
2025-05-01 23:16:12 +0200

    fix(cors): usar allowedOrigins en lugar de allowedOriginPatterns

--- src/main/java/com/swapfy/backend/config/SecurityConfig.java
+++ src/main/java/com/swapfy/backend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(
+                        .allowedOrigins(
                                 "http://localhost:4200",
                                 "https://swapfy-frontend.vercel.app"
                         )
@@ -44,6 +44,7 @@ public class SecurityConfig {
             }
         };
     }
+
 
 
     @Bean
