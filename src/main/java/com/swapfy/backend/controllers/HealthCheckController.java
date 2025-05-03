9831c3d9ae3fdb03ad8d1ae78c98ca4160167c46
Marr03 <msiegut@g.educaand.es>
2025-05-03 18:03:44 +0200

    Add: controlador de salud para evitar error 502 en Railway

---
+++ src/main/java/com/swapfy/backend/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.swapfy.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Swapfy backend is running!";
+    }
+}
